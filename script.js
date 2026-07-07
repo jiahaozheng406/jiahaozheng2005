fe165a5fe1f3ec5e64c8c55ee53e94af7aa2ba78
Zjh <jiahaozheng406@gmail.com>
2026-07-07 20:38:00 +0800

    Slightly enhance particle background

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ tsParticles.load("tsparticles", {
 
   particles: {
     number: {
-      value: 80,
+      value: 95,
       density: {
         enable: true,
         area: 800
@@ -21,31 +21,31 @@ tsParticles.load("tsparticles", {
     },
 
     color: {
-      value: "#9ca3af"
+      value: ["#60a5fa", "#38bdf8", "#a78bfa"]
     },
 
     links: {
       enable: true,
-      distance: 140,
-      color: "#d1d5db",
-      opacity: 0.35,
+      distance: 145,
+      color: "#bfdbfe",
+      opacity: 0.42,
       width: 1
     },
 
     move: {
       enable: true,
-      speed: 0.6,
+      speed: 0.75,
       outModes: {
         default: "bounce"
       }
     },
 
     opacity: {
-      value: 0.6
+      value: 0.68
     },
 
     size: {
-      value: { min: 1, max: 3 }
+      value: { min: 1, max: 3.4 }
     }
   },
 
@@ -53,15 +53,21 @@ tsParticles.load("tsparticles", {
     events: {
       onHover: {
         enable: true,
-        mode: "repulse"
+        mode: ["repulse", "grab"]
       },
       resize: true
     },
 
     modes: {
       repulse: {
-        distance: 90,
+        distance: 92,
         duration: 0.4
+      },
+      grab: {
+        distance: 165,
+        links: {
+          opacity: 0.48
+        }
       }
     }
   },
